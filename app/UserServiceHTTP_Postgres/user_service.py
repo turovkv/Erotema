aa9a7edef9d0525034c29db8050b2bc4049ad560
kirill <turovkv@yandex.ru>
2021-10-19 02:30:36 +0300

    add test db

--- app/UserServiceHTTP_Postgres/user_service.py
+++ app/UserServiceHTTP_Postgres/user_service.py
@@ -42,3 +42,10 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
 
+
+@router.get("/users/{email}", response_model=schemas.User)
+def read_user(email: str, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=email)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
