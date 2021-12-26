57732bd9f9c461045abc93ccfc66dacc896c12d6
kirill <turovkv@yandex.ru>
2021-12-26 16:54:38 +0300

    Add new endpoints

--- app/main/endpoint/api.py
+++ app/main/endpoint/api.py
@@ -44,3 +44,28 @@ def get_saved(user: User = Depends(get_authorised_user), quiz_service: QuizServi
 @router.get("/quiz/public", response_model=List[Quiz])
 def get_public_quizzes(user: User = Depends(get_authorised_user), quiz_service: QuizService = Depends()):
     return quiz_service.get_public_quizzes()
+
+
+@router.put("/quiz/user_session/points")
+def update_points(self, user_session_id: int, point_sum: int) -> None:
+    return self.repo.update_points(user_session_id, point_sum)
+
+
+@router.put("/quiz/publicity")
+def update_quiz_publicity(self, user_id: int, quiz_id: int) -> None:
+    return self.repo.update_quiz_publicity(user_id, quiz_id)
+
+
+@router.post("/quiz")
+def create_quiz(self, user_id: int, quiz: Quiz) -> int:
+    return self.repo.create_quiz(user_id, quiz)
+
+
+@router.post("/quiz_session")
+def create_quiz_session(self, user_id: int, quiz_id: int) -> int:
+    return self.repo.create_quiz_session(user_id, quiz_id)
+
+
+@router.post("/user_session")
+def create_user_session(self, user_id: int, session_id: int) -> int:
+    return self.repo.create_user_session(user_id, session_id)
